ad443e6c50351a33cd43ef52e18b56d27cb4e605
Owen Rubel <orubel@gmail.com>
2015-11-05 12:08:19 -0800

    removing testing logging

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -24,9 +24,6 @@ if(Environment.current == Environment.DEVELOPMENT) {
             }
         }
         logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false )
-        logger("org.springframework.security",INFO,['FULL_STACKTRACE'],false)
-        logger('grails.plugin.springsecurity', INFO,['FULL_STACKTRACE'],false)
-        logger('org.springframework.security.web.access.intercept', INFO, ['STDOUT'], false)
     }
 }
 
