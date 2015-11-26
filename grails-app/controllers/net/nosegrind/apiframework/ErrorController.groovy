d5777b9b070ac921aee88fc19c92c2ba5f193e92
Owen Rubel <orubel@gmail.com>
2015-11-26 08:48:12 -0800

    fixing errorcontroller

--- grails-app/controllers/net/nosegrind/apiframework/ErrorController.groovy
+++ grails-app/controllers/net/nosegrind/apiframework/ErrorController.groovy
@@ -1,15 +1,11 @@
 package net.nosegrind.apiframework
 
-/**
- * Created by owenrubel on 11/23/15.
- */
+
 class ErrorController {
 
     def index() {
-        def exception = request.exception.cause
-        def message = ExceptionMapper.mapException(exception)
+        def message = request.exception.cause.message
         def status = message.status
-        
-        render(status:status, text: message)
+        render( status: status, text: "${message}")
     }
 }
