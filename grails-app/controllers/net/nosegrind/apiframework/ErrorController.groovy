9c7a996d7aafa41f8b0c6fe7f972a9b31a8a22c8
Owen Rubel <orubel@gmail.com>
2015-12-01 10:28:49 -0800

    saving changes to errorcontroller

--- grails-app/controllers/net/nosegrind/apiframework/ErrorController.groovy
+++ grails-app/controllers/net/nosegrind/apiframework/ErrorController.groovy
@@ -4,7 +4,7 @@ package net.nosegrind.apiframework
 class ErrorController {
 
     def index() {
-        def error = request.exception.cause.message
+        def error = (request.exception?.cause?.message)?request.exception.cause.message:"NullPointerException"
         def status = (response?.status)? response?.status: id
         response.status = status
         render( status: status, text: "${error}")
